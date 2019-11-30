74883e6ba54be5acfd3e889824af609ad19f62e9
Maxime <maxime.minta@student.ap.be>
2019-11-30 13:47:03 +0100

    SoundboardActivity toegevoegd

---
+++ MobileDevApp/app/src/main/java/com/example/mobiledevapp/SoundBoardActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobiledevapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SoundBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sound_board);
+    }
+}
