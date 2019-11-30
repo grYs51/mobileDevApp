0c585ae9eb63389dd75b85be3b84eb45e8c8ff80
Maxime <maxime.minta@student.ap.be>
2019-11-30 20:37:26 +0100

    ActivitySoundboad + ik finish morgen

---
+++ MobileDevApp/app/src/main/java/com/example/mobiledevapp/SoundboardRecyclerAdapter.java
@@ -0,0 +1,23 @@
+package com.example.mobiledevapp;
+
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SoundboardRecyclerAdapter extends RecyclerView.Adapter<SoundBoardViewHolder> {
+
+@Override
+    public SoundBoardViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+    return null;
+}
+
+@Override
+    public void onBindViewHolder(SoundBoardViewHolder holder, int position){
+
+}
+
+@Override
+    public  int getItemCount(){
+    return 0;
+}
+}
