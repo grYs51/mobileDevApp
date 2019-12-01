ad46b78a6b6f69de92e18c5f95fba3264b8d92fc
Yves Bulckens <Yvesb@live.be>
2019-12-01 15:45:20 +0100

    Revert "added horizontal recycler view"
    This reverts commit 856ca5bdf3eaf8ee98d89a159a21268139bc625b.

--- MobileDevApp/app/src/main/java/com/example/mobiledevapp/SoundBoardActivity.java
+++ MobileDevApp/app/src/main/java/com/example/mobiledevapp/SoundBoardActivity.java
@@ -2,72 +2,44 @@ package com.example.mobiledevapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
-
-import java.util.ArrayList;
 
 
-public class SoundBoardActivity extends AppCompatActivity implements SoundboardRecyclerAdapter.ItemClickListener {
+public class SoundBoardActivity extends AppCompatActivity {
 
     public boolean[] listOptions;
     public static final String EXTRA_REPLY = "com.example.smalltalkapp.extra.REPLY";
-    private SoundboardRecyclerAdapter adapter;
+
+
+    String[] soundList = {"test1", "test2", "test3"};
+
+    RecyclerView SoundView;
+    SoundboardRecyclerAdapter SoundAdapter = new SoundboardRecyclerAdapter(soundList);
+    RecyclerView.LayoutManager SoundLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soundboard);
 
-        //toolbar
         Toolbar myToolbar = findViewById(R.id.toolbar_soundboard); //Shows name of app in the toolbar.
         setSupportActionBar(myToolbar);
         assert getSupportActionBar() != null;   //null check
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //RecyclerView 1
+        SoundView = (RecyclerView) findViewById(R.id.soundboardRecyclerView);
 
-        ArrayList<Integer> viewSounds = new ArrayList<>();
-        viewSounds.add(Color.WHITE);
-        viewSounds.add(Color.GRAY);
-        viewSounds.add(Color.GREEN);
-        viewSounds.add(Color.BLUE);
-        viewSounds.add(Color.YELLOW);
-        viewSounds.add(Color.MAGENTA);
-        viewSounds.add(Color.RED);
-        viewSounds.add(Color.CYAN);
+        SoundLayoutManager = new GridLayoutManager(this, 3);
+        SoundView.setLayoutManager(SoundLayoutManager);
+        SoundView.setAdapter(SoundAdapter);
 
-        ArrayList<String> soundNames = new ArrayList<>();
-        soundNames.add("sound 1");
-        soundNames.add("sound 2");
-        soundNames.add("sound 3");
-        soundNames.add("sound 4");
-        soundNames.add("sound 5");
-        soundNames.add("sound 6");
-        soundNames.add("sound 7");
-        soundNames.add("sound 8");
 
-        //setup recyclerView
 
-        RecyclerView recyclerView = findViewById(R.id.soundboardRecyclerView);
-        LinearLayoutManager horizontalLayoutManager
-                = new LinearLayoutManager(SoundBoardActivity.this, LinearLayoutManager.HORIZONTAL, false);
-        recyclerView.setLayoutManager(horizontalLayoutManager);
-        adapter = new SoundboardRecyclerAdapter(this, viewSounds, soundNames);
-        adapter.setClickListener(this);
-        recyclerView.setAdapter(adapter);
-
-//        SoundView = (RecyclerView) findViewById(R.id.soundboardRecyclerView);
-//        SoundLayoutManager = new GridLayoutManager(this, 3);
-//        SoundView.setLayoutManager(SoundLayoutManager);
-//        SoundView.setAdapter(SoundAdapter);
     }
 
     @Override
@@ -77,11 +49,5 @@ public class SoundBoardActivity extends AppCompatActivity implements SoundboardR
         setResult(RESULT_OK,replyIntent);
         finish();
         return true;
-    }
-
-    @Override
-    public void onItemClick(View view, int position) {
-
-        Toast.makeText(this, "playing sound: " + adapter.getItem(position), Toast.LENGTH_SHORT).show();
     }
 }
--- MobileDevApp/app/src/main/java/com/example/mobiledevapp/SoundboardRecyclerAdapter.java
+++ MobileDevApp/app/src/main/java/com/example/mobiledevapp/SoundboardRecyclerAdapter.java
@@ -1,116 +1,45 @@
 package com.example.mobiledevapp;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.List;
+public class SoundboardRecyclerAdapter extends RecyclerView.Adapter<SoundboardRecyclerAdapter.SoundBoardViewHolder> {
 
-public class SoundboardRecyclerAdapter extends RecyclerView.Adapter<SoundboardRecyclerAdapter.ViewHolder> {
+    private String[] soundObjects;
+    public SoundboardRecyclerAdapter(String[] SoundObjects){
+        this.soundObjects = SoundObjects;
+    };
+@Override
+    public SoundBoardViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
 
-//    private String[] soundObjects;
-//    public SoundboardRecyclerAdapter(String[] SoundObjects){
-//        this.soundObjects = SoundObjects;
-//    };
-//@Override
-//    public SoundBoardViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
-//
-//    View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.sound_item,null);
-//
-//    return new SoundBoardViewHolder(itemView);
-//}
-//
-//@Override
-//    public void onBindViewHolder(SoundBoardViewHolder holder, int position){
-//
-//    holder.itemTextView.setText(soundObjects[position]);
-//}
-//
-//@Override
-//    public  int getItemCount(){
-//
-//    return soundObjects.length;
-//}
-//
-//public class SoundBoardViewHolder extends RecyclerView.ViewHolder {
-//
-//    TextView itemTextView;
-//
-//    public SoundBoardViewHolder(View itemView) {
-//        super(itemView);
-//        itemTextView = (TextView) itemView.findViewById(R.id.TextViewItem);
-//    }
-//}
+    View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.sound_item,null);
 
-    private List<Integer> soundObjects;
-    private List<String> TextViewItem;
-    private LayoutInflater mInflater;
-    private ItemClickListener mClickListener;
-
-    // data is passed into the constructor
-    SoundboardRecyclerAdapter(Context context, List<Integer> sounds, List<String> soundName) {
-        this.mInflater = LayoutInflater.from(context);
-        this.soundObjects = sounds;
-        this.TextViewItem = soundName;
-    }
-
-    // inflates the row layout from xml when needed
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = mInflater.inflate(R.layout.sound_item, parent, false);
-        return new ViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        int color = soundObjects.get(position);
-        String soundName = TextViewItem.get(position);
-        holder.myView.setBackgroundColor(color);
-        holder.myTextView.setText(soundName);
-    }
-
-    @Override
-    public int getItemCount() {
-        return TextViewItem.size();
-    }
-
-    // stores and recycles views as they are scrolled off screen
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        View myView;
-        TextView myTextView;
-
-        ViewHolder(View itemView) {
-            super(itemView);
-            myView = itemView.findViewById(R.id.ImageViewItem);
-            myTextView = itemView.findViewById(R.id.TextViewItem);
-            itemView.setOnClickListener(this);
-        }
-
-        @Override
-        public void onClick(View view) {
-            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
-        }
-    }
-
-    // convenience method for getting data at click position
-    public String getItem(int id) {
-        return TextViewItem.get(id);
-    }
-
-    // allows clicks events to be caught
-    public void setClickListener(ItemClickListener itemClickListener) {
-        this.mClickListener = itemClickListener;
-    }
-
-    // parent activity will implement this method to respond to click events
-    public interface ItemClickListener {
-        void onItemClick(View view, int position);
-    }
+    return new SoundBoardViewHolder(itemView);
 }
 
+@Override
+    public void onBindViewHolder(SoundBoardViewHolder holder, int position){
+
+    holder.itemTextView.setText(soundObjects[position]);
+}
+
+@Override
+    public  int getItemCount(){
+
+    return soundObjects.length;
+}
+
+public class SoundBoardViewHolder extends RecyclerView.ViewHolder {
+
+    TextView itemTextView;
+
+    public SoundBoardViewHolder(View itemView) {
+        super(itemView);
+        itemTextView = (TextView) itemView.findViewById(R.id.TextViewItem);
+    }
+}
+}
