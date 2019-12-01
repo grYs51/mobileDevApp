856ca5bdf3eaf8ee98d89a159a21268139bc625b
Yves Bulckens <Yvesb@live.be>
2019-12-01 15:45:20 +0100

    added horizontal recycler view

--- MobileDevApp/app/src/main/java/com/example/mobiledevapp/SoundboardRecyclerAdapter.java
+++ MobileDevApp/app/src/main/java/com/example/mobiledevapp/SoundboardRecyclerAdapter.java
@@ -1,45 +1,116 @@
 package com.example.mobiledevapp;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class SoundboardRecyclerAdapter extends RecyclerView.Adapter<SoundboardRecyclerAdapter.SoundBoardViewHolder> {
+import java.util.List;
 
-    private String[] soundObjects;
-    public SoundboardRecyclerAdapter(String[] SoundObjects){
-        this.soundObjects = SoundObjects;
-    };
-@Override
-    public SoundBoardViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+public class SoundboardRecyclerAdapter extends RecyclerView.Adapter<SoundboardRecyclerAdapter.ViewHolder> {
 
-    View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.sound_item,null);
+//    private String[] soundObjects;
+//    public SoundboardRecyclerAdapter(String[] SoundObjects){
+//        this.soundObjects = SoundObjects;
+//    };
+//@Override
+//    public SoundBoardViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+//
+//    View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.sound_item,null);
+//
+//    return new SoundBoardViewHolder(itemView);
+//}
+//
+//@Override
+//    public void onBindViewHolder(SoundBoardViewHolder holder, int position){
+//
+//    holder.itemTextView.setText(soundObjects[position]);
+//}
+//
+//@Override
+//    public  int getItemCount(){
+//
+//    return soundObjects.length;
+//}
+//
+//public class SoundBoardViewHolder extends RecyclerView.ViewHolder {
+//
+//    TextView itemTextView;
+//
+//    public SoundBoardViewHolder(View itemView) {
+//        super(itemView);
+//        itemTextView = (TextView) itemView.findViewById(R.id.TextViewItem);
+//    }
+//}
 
-    return new SoundBoardViewHolder(itemView);
-}
+    private List<Integer> soundObjects;
+    private List<String> TextViewItem;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
 
-@Override
-    public void onBindViewHolder(SoundBoardViewHolder holder, int position){
+    // data is passed into the constructor
+    SoundboardRecyclerAdapter(Context context, List<Integer> sounds, List<String> soundName) {
+        this.mInflater = LayoutInflater.from(context);
+        this.soundObjects = sounds;
+        this.TextViewItem = soundName;
+    }
 
-    holder.itemTextView.setText(soundObjects[position]);
-}
+    // inflates the row layout from xml when needed
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.sound_item, parent, false);
+        return new ViewHolder(view);
+    }
 
-@Override
-    public  int getItemCount(){
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        int color = soundObjects.get(position);
+        String soundName = TextViewItem.get(position);
+        holder.myView.setBackgroundColor(color);
+        holder.myTextView.setText(soundName);
+    }
 
-    return soundObjects.length;
-}
+    @Override
+    public int getItemCount() {
+        return TextViewItem.size();
+    }
 
-public class SoundBoardViewHolder extends RecyclerView.ViewHolder {
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        View myView;
+        TextView myTextView;
 
-    TextView itemTextView;
+        ViewHolder(View itemView) {
+            super(itemView);
+            myView = itemView.findViewById(R.id.ImageViewItem);
+            myTextView = itemView.findViewById(R.id.TextViewItem);
+            itemView.setOnClickListener(this);
+        }
 
-    public SoundBoardViewHolder(View itemView) {
-        super(itemView);
-        itemTextView = (TextView) itemView.findViewById(R.id.TextViewItem);
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    // convenience method for getting data at click position
+    public String getItem(int id) {
+        return TextViewItem.get(id);
+    }
+
+    // allows clicks events to be caught
+    public void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
     }
 }
-}
+
