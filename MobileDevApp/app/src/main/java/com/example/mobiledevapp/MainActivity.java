61944108e75d479d87417a29e268e405e6791344
Yves Bulckens <Yvesb@live.be>
2019-12-09 14:08:47 +0100

    sharedPreff

--- MobileDevApp/app/src/main/java/com/example/mobiledevapp/MainActivity.java
+++ MobileDevApp/app/src/main/java/com/example/mobiledevapp/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.mobiledevapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.preference.PreferenceManager;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -17,11 +20,35 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.smalltalkapp.extra.MESSAGE";
     public static int counter;
+    public static SharedPreferences settings;
+    private String sharedPrefFile = "com.example.mobiledevapp";
+
+
     public boolean[] listOptions={true,true,true};
     public static final int BOOL_REQUEST = 1;
     private FirebaseAnalytics mFirebaseAnalytics;
 
+
+
+
+
+
+
     public static TextView tvCounter;
+
+
+    public static void saveCount() {
+
+
+        counter = counter + 1;
+
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt("counter", counter);
+
+        editor.commit();
+        MainActivity.tvCounter.setText(Integer.toString(counter));
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +59,11 @@ public class MainActivity extends AppCompatActivity {
         // Obtain the FirebaseAnalytics instance.
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
+        SharedPreferences settings = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+        counter = settings.getInt("counter", 0);
+
          tvCounter = findViewById(R.id.tvCounter);
-        tvCounter.setText("0");
+        tvCounter.setText(Integer.toString(counter)  );
     }
 
 
